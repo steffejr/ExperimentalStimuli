b85c3c3f2a91b5b42aeff13e26f21e223f328a4a
steffejr <steffener@gmail.com>
2014-10-02 14:23:15 -0400

    N-back is complete and works.
    It does ned more features like multiple load levels and to start with a
    trigger from the MRI. Right now it only does load level 2.

--- NBack/NBackv1.py
+++ NBack/NBackv1.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
 from psychopy import visual, core, data, event, logging, sound, gui
 from psychopy.constants import *  # things like STARTED, FINISHED
@@ -7,6 +7,9 @@ from numpy import sin, cos, tan, log, log10, pi, average, sqrt, std, deg2rad, ra
 from numpy.random import random, randint, normal, shuffle
 import os  # handy system and path functions
 from psychopy.hardware.emulator import launchScan
+import numpy as np
+from NBackFunctions import *
+
 
 # EXPERIMENTAL INFORMATION
 expName = u'NBack'  # <<< This is where you specify the name of the experiment which makes it easier to differentiate the results files.
@@ -23,7 +26,7 @@ filename = 'data/%s_%s_%s' %(expInfo['participant'], expName, expInfo['date'])
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
     originPath=None,
-    savePickle=True, saveWideText=True,
+    savePickle=False, saveWideText=True,
     dataFileName=filename)
 #save a log file for detail verbose info
 logFile = logging.LogFile(filename+'.log', level=logging.EXP)
@@ -62,12 +65,17 @@ else:
 # Create an N-back task
 ExpParameters = {
     'NTrials': 60,
-    'NBlocks': 5,
+    'NBlocks': 2,
     'LoadLevels': range(0,3),
     'TimePerTrial': 2, # seconds
     'TrialPerBlock': 12,
-    'StimList': 'ABCDEFGH',
-    'NumCorrectPerBlock': 4
+    'StimList': 'ABCDEFGHJKLMNPRSTVWXYZ',
+    'ResponseKeys':['1','2','3','4','5','6','7','8','9','0'],
+    'NumCorrectPerBlock': 4,
+    'IntroOffDuration': 5,
+    'OffDuration' : 5,
+    'OnDuration' : 5,
+    'InstrTime': 6
 }
 
 TextSize = 0.2
@@ -84,88 +92,103 @@ CrossHair = visual.TextStim(win=win, ori=0, name='text',
     color=u'red', colorSpace=u'rgb', opacity=1,
     depth=0.0)
     
-
-# Create stimuli
-def CreateStim(LoadLevel,TrialPerBlock,NumCorrectPerBlock):
-    # pick NumCorPerBlock unique numbers between 1 and TrialPerBlock
-    # make sure that the numbers are seperated by at least the load level number of items 
-    AllTrue = False
-    count = 0
-    SuccessFlag = False
-    while (AllTrue == False) and count < 1000:
-        test = np.round(np.random.uniform(TrialPerBlock*np.ones(NumCorrectPerBlock)))
-        test.sort()
-        UniqueFlag = len(set(test)) == len(test)
-        FirstEntryFlag = test[0] > LoadLevel
-        SpacingFlag = True
-        PreviousLoc = test[0]
-        for i in range(1,len(test)):
-            if test[i] < test[i-1] + LoadLevel:
-                SpacingFlag = False
-        # Make sure all conditions are true
-        AllTrue = UniqueFlag == True & FirstEntryFlag == True & SpacingFlag == True
-        count += 1
-        
-    if AllTrue == True:
-        print "It took %d attempts to create this list"%count
-        return test
-    else:
-        print "Could not find a solution in %d attempts"%count
-        return -99
-    
-
-def AssignStimuli(CorrectLocations,TrialPerBlock,Stimuli,LoadLevel):
-    List = ['-99']*TrialPerBlock
-    NCor = len(CorrectLocations)
-    NStim = len(Stimuli)
-    # Place random stimuli in the correct locations
-    for i in CorrectLocations:
-        RandomPick = int(np.round(np.random.uniform(1*NStim)))
-        List[int(i)-1] = Stimuli[RandomPick-1]
-    # go through and set the previous stimuli appropriately
-    for i in range(0,TrialPerBlock,1):
-        if List[i] != '-99':
-            if List[i-LoadLevel] != '-99':
-                List[i] = List[i-LoadLevel]
-            else:
-                List[i-LoadLevel] = List[i]
-    # Find the unused stimuli
-    UnusedStimuli = []
-    for i in Stimuli:
-        if i not in List:
-            UnusedStimuli.append(i)
-    # fill in the empty trials
-    for i in range(0,TrialPerBlock,1):
-        if List[i] == '-99':
-            RandomPick = int(np.round(np.random.uniform(1*len(UnusedStimuli))))
-            # remove the stimuli from the list
-            List[i] = UnusedStimuli[RandomPick-1]
-            UnusedStimuli.pop(RandomPick-1)
-    return List
+InstrLevel2 = visual.ImageStim(win,image='2BackInstructions.jpg',
+    mask=None,
+    pos=(0.0,0.0),
+    size=(1.0,1.0))
 
 
-LoadLevel = 2
-CorrectLocations = CreateStim(LoadLevel,ExpParameters['TrialPerBlock'],ExpParameters['NumCorrectPerBlock'])
-List = AssignStimuli(CorrectLocations,ExpParameters['TrialPerBlock'],ExpParameters['StimList'],LoadLevel)
-
+resp = event.BuilderKeyResponse()  # create an object of type KeyResponse
+#Instructions
+TrialClock = core.Clock()
+TrialClock.reset()
+CountDownClock = core.CountdownTimer()
 ElapsedTimeClock = core.Clock()
-# present a block of stimuli
-for item in List:
-    StimulusText = visual.TextStim(win=win, ori=0, name='text',
-            text=item,    font=u'Arial',
-            pos=[0, 0], height=TextSize, wrapWidth=None,
-            color=u'white', colorSpace=u'rgb', opacity=1,
-            depth=0.0)    
-    StimulusText.draw()
-    win.flip()
-    ElapsedTimeClock.reset()
-    while ElapsedTimeClock.getTime() < 1:
-        if event.getKeys(keyList=["escape"]):
-            core.quit() 
-        else:
-            # This must be a response
-            print event.getKeys()
+
+# Cross hair
+ElapsedTimeClock.reset()
+CountDownClock.reset()
+CountDownClock.add(ExpParameters['IntroOffDuration'])
 CrossHair.draw()
 win.flip()
+while CountDownClock.getTime() > 0:
+    theseKeys = event.getKeys()
+    if "escape" in theseKeys:
+        win.flip()
+        win.close()
+        core.quit() 
+for BlockNumber in range(0,ExpParameters['NBlocks'],1):
+    LoadLevel = 2
+    CorrectLocations = CreateStim(LoadLevel,ExpParameters['TrialPerBlock'],ExpParameters['NumCorrectPerBlock'])
+    List = AssignStimuli(CorrectLocations,ExpParameters['TrialPerBlock'],ExpParameters['StimList'],LoadLevel)
+                            
+    # Instructions
+    CountDownClock.add(ExpParameters['InstrTime'])
+    thisExp.addData('Stimulus','Instructions')
+    thisExp.addData('ElapsedTime',ElapsedTimeClock.getTime())
+    thisExp.nextEntry()
+    TrialClock.reset()
+    InstrLevel2.draw()   
+    win.flip()
+    while CountDownClock.getTime() > 0:
+        theseKeys = event.getKeys()
+        if "escape" in theseKeys:
+            win.flip()
+            win.close()
+            core.quit() 
+            
+                
+    # present a block of stimuli
+    count = 0
+    print CorrectLocations
+    for item in List:
+        CountDownClock.add(ExpParameters['TimePerTrial'])
+        StimulusText = visual.TextStim(win=win, ori=0, name='text',
+                text=item,    font=u'Arial',
+                pos=[0, 0], height=TextSize, wrapWidth=None,
+                color=u'white', colorSpace=u'rgb', opacity=1,
+                depth=0.0)    
+        StimulusText.draw()
+        win.flip()
+        TrialClock.reset()
+        thisExp.addData('ElapsedTime',ElapsedTimeClock.getTime())
+        while CountDownClock.getTime() > 0:
+            theseKeys = event.getKeys()
+            if "escape" in theseKeys:
+                win.flip()
+                win.close()
+                core.quit() 
+            elif len(theseKeys) > 0:
+                # This must be a response
+                CurrentRT = TrialClock.getTime()
+                thisExp.addData('KeyPress',theseKeys[-1])
+                thisExp.addData('RT',CurrentRT)
+                if count in CorrectLocations:
+                    print "TRUE"
+                    thisExp.addData('Correct','Y')
+                else:
+                    thisExp.addData('Correct','N')
+                resp.KeyPress = theseKeys[-1]
+                resp.RT = CurrentRT
+                CurrentRT = TrialClock.getTime()
+                print "%02d: %s Key press: %s in %0.4f sec"%(count,item,theseKeys[-1],CurrentRT)
+        count += 1
+        thisExp.addData('count',count)
+        thisExp.addData('Block',BlockNumber)
+        thisExp.addData('Stimulus',item)
+        thisExp.addData('LoadLevel',LoadLevel)
+        thisExp.nextEntry()
+    
+    CountDownClock.add(ExpParameters['OffDuration'])        
+    CrossHair.draw()
+    win.flip()
+    while TrialClock.getTime() < 4:
+        theseKeys = event.getKeys()
+        if "escape" in theseKeys:
+            win.flip()
+            core.quit() 
+
+win.close()
+core.quit()
         
     
